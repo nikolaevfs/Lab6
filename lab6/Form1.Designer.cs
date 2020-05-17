0472c4dec3a54a0f745a75cb37bc34edd5c65f7c
UncleFedor527 <56272782+UncleFedor527@users.noreply.github.com>
2020-05-18 01:58:20 +0300

    (sender as Button).Text

--- lab6/Form1.Designer.cs
+++ lab6/Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "8";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -110,7 +110,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "9";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
@@ -125,7 +125,7 @@
             this.button5.TabIndex = 7;
             this.button5.Text = "6";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.button1_Click);
             // 
             // button6
             // 
@@ -155,7 +155,7 @@
             this.button7.TabIndex = 5;
             this.button7.Text = "5";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.button1_Click);
             // 
             // button8
             // 
@@ -170,7 +170,7 @@
             this.button8.TabIndex = 4;
             this.button8.Text = "4";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.button1_Click);
             // 
             // button9
             // 
@@ -185,7 +185,7 @@
             this.button9.TabIndex = 11;
             this.button9.Text = "3";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button1_Click);
             // 
             // button10
             // 
@@ -215,7 +215,7 @@
             this.button11.TabIndex = 9;
             this.button11.Text = "2";
             this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
+            this.button11.Click += new System.EventHandler(this.button1_Click);
             // 
             // button12
             // 
@@ -230,7 +230,7 @@
             this.button12.TabIndex = 8;
             this.button12.Text = "1";
             this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
+            this.button12.Click += new System.EventHandler(this.button1_Click);
             // 
             // button13
             // 
@@ -245,7 +245,7 @@
             this.button13.TabIndex = 15;
             this.button13.Text = ",";
             this.button13.UseVisualStyleBackColor = true;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
+            this.button13.Click += new System.EventHandler(this.button1_Click);
             // 
             // button14
             // 
@@ -275,7 +275,7 @@
             this.button15.TabIndex = 13;
             this.button15.Text = "0";
             this.button15.UseVisualStyleBackColor = true;
-            this.button15.Click += new System.EventHandler(this.button15_Click);
+            this.button15.Click += new System.EventHandler(this.button1_Click);
             // 
             // button16
             // 
@@ -315,10 +315,11 @@
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(586, 94);
+            this.textBox1.Size = new System.Drawing.Size(586, 57);
             this.textBox1.TabIndex = 17;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // tableLayoutPanel1
             // 
--- lab6/Form1.cs
+++ lab6/Form1.cs
@@ -21,57 +21,7 @@ namespace lab6
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 7;
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 2;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 5;
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 0;
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + ",";
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 1;
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 3;
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 4;
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 6;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 8;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 9;
+            textBox1.Text = textBox1.Text +(sender as Button).Text;
         }
 
         private void button3_Click(object sender, EventArgs e)
